cc6e7c5199c04ceeeba30348d0bcc69f671dcd42
lilyhabti <105781552+lilyhabti@users.noreply.github.com>
2025-05-18 23:54:09 +0100

    part security implemented but still have some errors to fix

--- backend/src/main/java/ma/enset/ebanking_digital/web/CustomerRestController.java
+++ backend/src/main/java/ma/enset/ebanking_digital/web/CustomerRestController.java
@@ -1,45 +1,59 @@
 package ma.enset.ebanking_digital.web;
 
-
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-
 import ma.enset.ebanking_digital.dtos.CustomerDTO;
 import ma.enset.ebanking_digital.exceptions.CustomerNotFoundException;
 import ma.enset.ebanking_digital.services.BankAccountService;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+// c'est un web service
 @RestController
 @AllArgsConstructor
 @Slf4j
-@CrossOrigin("*")
+// Permet de gérer les requêtes provenant de domaines différents (CORS - Cross-Origin Resource Sharing)
+@CrossOrigin("*") // autorisé tout les domaines
 public class CustomerRestController {
+
     private BankAccountService bankAccountService;
+
     @GetMapping("/customers")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public List<CustomerDTO> customers(){
         return bankAccountService.listCustomers();
     }
-    @GetMapping("/customers/search")
-    public List<CustomerDTO> searchCustomers(@RequestParam(name = "keyword",defaultValue = "") String keyword){
-        return bankAccountService.searchCustomers("%"+keyword+"%");
-    }
+
     @GetMapping("/customers/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_USER')")
     public CustomerDTO getCustomer(@PathVariable(name = "id") Long customerId) throws CustomerNotFoundException {
         return bankAccountService.getCustomer(customerId);
     }
+
     @PostMapping("/customers")
-    public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO){
+    @PostAuthorize("hasAuthority('SCOPE_ADMIN')")
+    public CustomerDTO saveCustomer(@RequestBody CustomerDTO customerDTO) {
         return bankAccountService.saveCustomer(customerDTO);
     }
+
     @PutMapping("/customers/{customerId}")
-    public CustomerDTO updateCustomer(@PathVariable Long customerId, @RequestBody CustomerDTO customerDTO){
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
+    public CustomerDTO updateCustomer(@PathVariable Long customerId,@RequestBody CustomerDTO customerDTO) {
         customerDTO.setId(customerId);
         return bankAccountService.updateCustomer(customerDTO);
     }
+
     @DeleteMapping("/customers/{id}")
+    @PreAuthorize("hasAuthority('SCOPE_ADMIN')")
     public void deleteCustomer(@PathVariable Long id){
         bankAccountService.deleteCustomer(id);
+    }
+
+    @GetMapping("/customers/search")
+    public List<CustomerDTO> searchCustomer(@RequestParam(name = "keyword", defaultValue = "") String keyword) {
+        return bankAccountService.searchCustomers("%"+keyword+"%");
     }
 }
--- frontend/digital-banking-front/src/app/accounts/accounts.component.ts
+++ frontend/digital-banking-front/src/app/accounts/accounts.component.ts
@@ -1,8 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 import { AccountService } from '../services/account.service';
-import { AccountDetails } from '../model/account.model';
 import { catchError, Observable, throwError } from 'rxjs';
+import { AccountDetails } from '../model/account.model';
+import Swal from 'sweetalert2';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-accounts',
@@ -10,31 +12,35 @@ import { catchError, Observable, throwError } from 'rxjs';
   templateUrl: './accounts.component.html',
   styleUrl: './accounts.component.css'
 })
-export class AccountsComponent implements OnInit {
-  accountFormGroup!: FormGroup;
-  currentPage: number = 0;
-  pageSize: number = 5;
-  accountObservable!: Observable<AccountDetails>;
-  operationFormGroup!: FormGroup;
-  errorMessage!: string;
+export class AccountsComponent implements OnInit{
 
-  constructor(private formBuilder: FormBuilder, private accountService: AccountService) { }
+  accountFormGroup! : FormGroup;
+  currentPage : number = 0;
+  pageSize : number = 5;
+  accountObservable! : Observable<AccountDetails>;
+  operationFormGroup! : FormGroup;
+  errorMessage! : string;
+
+
+  constructor(private fb : FormBuilder, private accountService : AccountService, public authService : AuthService){}
 
   ngOnInit(): void {
-    this.accountFormGroup = this.formBuilder.group({
-      accountId: this.formBuilder.control('')
+    this.accountFormGroup = this.fb.group({
+      accountId : this.fb.control('')
     });
-    this.operationFormGroup = this.formBuilder.group({
-      operationType: this.formBuilder.control(null),
-      amount: this.formBuilder.control(0),
-      description: this.formBuilder.control(null),
-      accountDestination: this.formBuilder.control(null)
-    })
+
+    this.operationFormGroup = this.fb.group({
+      operationType : this.fb.control(null),
+      amount : this.fb.control(0),
+      description : this.fb.control(null),
+      accountDestination : this.fb.control(null)
+    });
   }
 
-  handleSearchAccount() {
-    let accountId: string = this.accountFormGroup.value.accountId;
-    this.accountObservable = this.accountService.getAccount(accountId, this.currentPage, this.pageSize).pipe(
+  handleSearchAccount(){
+    let accountId : string = this.accountFormGroup.value.accountId;
+    // au lieux d'utilisé .subscibe....
+    this.accountObservable =  this.accountService.getAccount(accountId, this.currentPage, this.pageSize).pipe(
         catchError(err => {
           this.errorMessage = err.message;
           return throwError(err);
@@ -42,51 +48,66 @@ export class AccountsComponent implements OnInit {
     );
   }
 
-  gotoPage(page: number) {
+  goToPage(page : number){
     this.currentPage = page;
     this.handleSearchAccount();
   }
 
-  handleAccountOperation() {
-    let accountId: string = this.accountFormGroup.value.accountId;
+  handleAccountOperation(){
+    let accountId : string = this.accountFormGroup.value.accountId;
     let operationType = this.operationFormGroup.value.operationType;
-    let amount: number = this.operationFormGroup.value.amount;
-    let description: string = this.operationFormGroup.value.description;
-    let accountDestination: string = this.operationFormGroup.value.accountDestination;
-    if (operationType == 'DEBIT') {
+    let amount : number = this.operationFormGroup.value.amount;
+    let description : string = this.operationFormGroup.value.description;
+    let accountDestination : string = this.operationFormGroup.value.accountDestination;
+
+    if(operationType == 'DEBIT'){
       this.accountService.debit(accountId, amount, description).subscribe({
-        next: (data) => {
-          alert("Success Credit");
+        next : data =>{
+          Swal.fire({
+            title: "Success Debit ",
+            icon: "success",
+            draggable: true
+          });
           this.operationFormGroup.reset();
           this.handleSearchAccount();
-        },
-        error: (err) => {
-          console.log(err);
+        }, error : err =>{
+          console.log(err)
         }
-      });
-    } else if (operationType == 'CREDIT') {
-      this.accountService.credit(accountId, amount, description).subscribe({
-        next: (data) => {
-          alert("Success Debit");
-          this.operationFormGroup.reset();
-          this.handleSearchAccount();
-        },
-        error: (err) => {
-          console.log(err);
-        }
-      });
+      })
     }
-    else if (operationType == 'TRANSFER') {
-      this.accountService.transfer(accountId, accountDestination, amount, description).subscribe({
-        next: (data) => {
-          alert("Success Transfer");
+    else if(operationType == 'CREDIT'){
+
+      this.accountService.credit(accountId, amount, description).subscribe({
+        next : data =>{
+          Swal.fire({
+            title: "Success Credit ",
+            icon: "success",
+            draggable: true
+          });
           this.operationFormGroup.reset();
           this.handleSearchAccount();
-        },
-        error: (err) => {
-          console.log(err);
+        }, error : err =>{
+          console.log(err)
         }
-      });
+      })
+
+    } else if(operationType == 'TRANSFER'){
+
+      this.accountService.transfer(accountId, accountDestination,  amount, description).subscribe({
+        next : data =>{
+          Swal.fire({
+            title: "Success transfer ",
+            icon: "success",
+            draggable: true
+          });
+          this.operationFormGroup.reset();
+          this.handleSearchAccount();
+        }, error : err =>{
+          console.log(err)
+        }
+      })
     }
   }
+
+
 }
